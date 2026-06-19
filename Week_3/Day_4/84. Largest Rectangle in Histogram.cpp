b2d6e907f3b06e441f0cda9b6c568dc2903300e2
SHIKHARCHATURVEDI19 <shikhar7936@gmail.com>
2026-06-20 00:22:03 +0530

    lc

--- Week_3/Day_4/84. Largest Rectangle in Histogram.cpp
+++ Week_3/Day_4/84. Largest Rectangle in Histogram.cpp
@@ -30,7 +30,6 @@ Constraints:
 #include<vector>
 
 using namespace std;
-
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
@@ -39,13 +38,22 @@ public:
         int n = heights.size();
         
         for (int i = 0; i <= n; i++) {
-            int currentHeight = (i == n) ? 0 : heights[i];
+            int currentHeight = 0;
+            if (i < n) {
+                currentHeight = heights[i];
+            }
             
             while (!st.empty() && currentHeight < heights[st.top()]) {
                 int height = heights[st.top()];
                 st.pop();
                 
-                int width = st.empty() ? i : (i - st.top() - 1);
+                int width;
+                if (st.empty()) {
+                    width = i;
+                } else {
+                    width = i - st.top() - 1;
+                }
+                
                 maxArea = max(maxArea, height * width);
             }
             st.push(i);
